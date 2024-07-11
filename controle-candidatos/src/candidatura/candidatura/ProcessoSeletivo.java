ac8b111118593385f8f64e849957bf1a6e0514f2
nunorulz <nunorulz@gmail.com>
2024-07-11 12:32:38 -0300

    Processo Seletivo

---
+++ controle-candidatos/src/candidatura/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,21 @@
+package candidatura;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        analisarCandidato(1900.0);
+        analisarCandidato(2000.0);
+        analisarCandidato(2200.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato");
+        } else if (salarioBase == salarioPretendido)
+            System.out.println("Ligar para o candidato com uma contra proposta");
+        else {
+            System.out.println("Aguardando o resultado dos demais candidatos");
+        }
+
+    }
+}
